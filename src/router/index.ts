34c94047614d76c8bf3b4c01e4bda3d6f5beeae0
231250141 <231250141@smail.nju.edu.cn>
2025-03-28 19:42:50 +0800

    delete about

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,14 +25,6 @@ const router = createRouter({
       component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/account-settings',
       component: AccountSettings,
       children: [
