a2a4c06a331c9032d0794a53778679515911f814
231250141 <231250141@smail.nju.edu.cn>
2025-04-17 12:55:04 +0800

    modify env

--- src/views/AdminProductManagement/LeftSidebar.vue
+++ src/views/AdminProductManagement/LeftSidebar.vue
@@ -6,6 +6,7 @@ import { useRouter, useRoute } from 'vue-router'
 const menuItems = ref([
   { name: '商品管理', route: '/admin/product-management' },
   { name: '订单管理', route: '/admin/order-management' },
+  { name:  '广告管理'},
   { name: '用户管理', route: '/admin/user-management' },
   { name: '数据统计', route: '/admin/stats' },
   { name: '系统设置', route: '/admin/settings' }
